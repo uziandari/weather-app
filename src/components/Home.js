fd9068e462a0d45a89e50cf99cbcf0f1798f1d23
Steve M <uziandari@gmail.com>
2017-01-12 15:01:24 -0500

    weather card updated, some css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import '../css/home';
+
 export default class Home extends Component {
 
   constructor() {
@@ -19,9 +21,9 @@ export default class Home extends Component {
       <div className="home-wrapper">
         <div className="weather-box">
           <h1 className="city">City: {this.state.city}</h1>
-          <h2 className="country">{this.state.country}</h2>
-          <h3 className="temperature">{this.state.currentTemp}</h3>
-          <h2 className="weather">{this.state.currentWeather}</h2>
+          <p className="country">Country: {this.state.country}</p>
+          <h3 className="temperature">Temp: {this.state.currentTemp}</h3>
+          <h2 className="weather">Weather: {this.state.currentWeather}</h2>
         </div>
       </div>
     )
